99bfb59c94c4688a6c765cf57674611a92f3b236
Jose Zalacain <Jose Zalacain@10.11.0.30>
2018-10-28 02:06:17 +0100

    Rename providers

--- src/main/java/org/logicware/prolog/jpl7/swi/SwiPrologContainerFactory.java
+++ src/main/java/org/logicware/prolog/jpl7/swi/SwiPrologContainerFactory.java
@@ -3,12 +3,12 @@ package org.logicware.prolog.jpl7.swi;
 import org.logicware.pdb.HierarchicalCache;
 import org.logicware.pdb.Settings;
 import org.logicware.pdb.prolog.PrologContainerFactory;
-import org.logicware.prolog.jpl7.swi7.SwiPrologProvider;
+import org.logicware.prolog.jpl7.swi7.SwiProlog7;
 
 public final class SwiPrologContainerFactory extends PrologContainerFactory {
 
 	public SwiPrologContainerFactory(Settings settins) {
-		super(settins, new SwiPrologProvider());
+		super(settins, new SwiProlog7());
 	}
 
 	public HierarchicalCache createHierarchicalCache() {
