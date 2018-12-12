679454b2653a878ccd555615d4a190cd725112ae
Jose Zalacain <jzalacain@delnet.cu>
2018-12-12 15:00:11 +0100

    Update JPA related

---
+++ src/main/java/org/logicware/prolog/jpl7/swi/SwiPrologPersistenceProvider.java
@@ -0,0 +1,9 @@
+package org.logicware.prolog.jpl7.swi;
+
+import javax.persistence.spi.PersistenceProvider;
+
+import org.logicware.database.jpa.JpaAbstractProvider;
+
+public class SwiPrologPersistenceProvider extends JpaAbstractProvider implements PersistenceProvider {
+
+}
