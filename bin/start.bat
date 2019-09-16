a6778ec04f994a245353cea6f9a3e3ba427c3c61
Jose Zalacain <Jose Zalacain@DESKTOP-P1RGQ48>
2019-09-16 06:03:52 -0400

    Update contributing

--- bin/start.bat
+++ bin/start.bat
@@ -5,4 +5,5 @@ for %%B in (%CURRENT_DIRECTORY%.) do set PROLOBJECTLINK_HOME=%%~dpB
 SET CLASSPATH=%PROLOBJECTLINK_HOME%lib\*
 
 : default jdk
+java -classpath %CLASSPATH% org.prolobjectlink.db.prolog.jpl7.swi7.SwiPrologDatabaseConsole -m
 java -classpath %CLASSPATH% org.prolobjectlink.db.prolog.jpl7.swi7.SwiPrologDatabaseConsole -z 9110
--- bin/start.sh
+++ bin/start.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/bash
+java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl7.swi7.SwiPrologDatabaseConsole -m
 java -classpath "$(dirname "$(pwd)")/lib/*" org.prolobjectlink.db.prolog.jpl7.swi7.SwiPrologDatabaseConsole -z 9110
